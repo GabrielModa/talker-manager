e7a43ae8d75b592bb79a2b711575fc7f028d815f
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-02-19 03:51:46 -0300

    02 OK

--- controllers/getTalkerById.js
+++ controllers/getTalkerById.js
@@ -7,7 +7,7 @@ try {
   const talkers = await readFile('./talker.json', 'utf-8');
   const parsedTalkers = JSON.parse(talkers);
 
-  const talkerFind = parsedTalkers.find((t) => t.id == id);
+  const talkerFind = parsedTalkers.find((t) => t.id === Number(id));
   if (!talkerFind) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   
   return res.status(200).json(talkerFind);    
