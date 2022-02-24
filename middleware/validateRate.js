c3f196da9b41bc4e977a3db1bc331ebf99bc159e
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-02-24 04:35:00 -0300

    04 OK

--- middleware/validateRate.js
+++ middleware/validateRate.js
@@ -6,9 +6,9 @@ module.exports = (req, res, next) => {
       return res.status(400).json({ message:
         'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
     }
-    // if (parseInt(talk.rate, 10) >= 1 && parseInt(talk.rate, 10) <= 5) {
-    //   return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-    // }
+    if (parseInt(talk.rate, 10) < 1 || parseInt(talk.rate, 10) > 5) {
+      return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    }
     return next();
   } catch (e) {
     next(e);
